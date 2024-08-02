dc4fe4ddd125b9c8d1655a246ce9c94282ade8d4
Xiangchong Li <xiangchong.li@ipmu.jp>
2024-08-02 14:43:15 +0900

    readthedocs

--- python/anacal/__init__.py
+++ python/anacal/__init__.py
@@ -1,4 +1,5 @@
 from . import _anacal, base, fpfs, simulation
 from ._anacal import image, mask, model, noise, psf
+from .__version__ import __version__
 
 __all__ = ["_anacal", "image", "fpfs", "model", "base", "noise", "psf", "mask"]
