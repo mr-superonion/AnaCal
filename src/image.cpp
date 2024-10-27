248a4f3dd869239b1bc4ada53b72e9d282c38415
Xiangchong Li <xiangchong.li@ipmu.jp>
2024-10-27 11:13:44 +0900

    add unit test for the forced measurement

--- src/image.cpp
+++ src/image.cpp
@@ -412,26 +412,29 @@ Image::measure(
     for (int j = 0; j < ky_length; ++j) {
         int ji = j * kx_length;
         double kj = two_pi * (j <= ny / 2 ? j : j - ny) / ny;
-        double cosp = std::cos(kj * dy);
         for (int i = -1; i < 1; ++i) {
             int ii = (i + kx_length) % kx_length;
             int index = ji + ii;
             std::complex<double> val(data_f[index][0], data_f[index][1]);
             double ki = two_pi * ii / nx;
-            double sinp = std::sin(-1.0 * ki * dx);
-            std::complex<double> phase(cosp, sinp);
+            double phase = kj * dy + ki * dx;
+            std::complex<double> factor(
+                std::cos(phase), std::sin(phase)
+            );
             for (int z = 0; z < ncol; ++z) {
-                meas_r(z) = meas_r(z) + (fr(j, ii, z) * phase * val).real();
+                meas_r(z) = meas_r(z) + (fr(j, ii, z) * factor * val).real();
             }
         }
         for (int i = 1; i < kx_length - 1; ++i) {
             int index = ji + i;
             std::complex<double> val(data_f[index][0], data_f[index][1]);
             double ki = two_pi * i / nx;
-            double sinp = std::sin(-1.0 * ki * dx);
-            std::complex<double> phase(cosp, sinp);
+            double phase = kj * dy + ki * dx;
+            std::complex<double> factor(
+                std::cos(phase), std::sin(phase)
+            );
             for (int z = 0; z < ncol; ++z) {
-                meas_r(z) = meas_r(z) + (fr(j, i, z) * phase * val).real() * 2.0;
+                meas_r(z) = meas_r(z) + (fr(j, i, z) * factor * val).real() * 2.0;
             }
         }
     }
---
+++ tests/fpfs/test_fpfs_force_measure.py
@@ -0,0 +1,74 @@
+import anacal
+import galsim
+import numpy as np
+import numpy.lib.recfunctions as rfn
+
+ngrid = 64
+mag_zero = 27
+
+
+def simulate_gal_psf(scale, shift_x, shift_y):
+    psf_obj = galsim.Moffat(beta=3.5, fwhm=0.6, trunc=0.6 * 4.0).shear(
+        e1=0.02, e2=-0.02
+    )
+
+    psf_array = (
+        psf_obj.shift(0.5 * scale, 0.5 * scale)
+        .drawImage(nx=ngrid, ny=ngrid, scale=scale)
+        .array
+    )
+
+    gal_obj = galsim.Gaussian(fwhm=0.6).shear(
+        e1=0.2, e2=-0.24
+    )
+    gal_array = (
+        gal_obj.shift((0.5 + shift_x) * scale, (0.5 + shift_y) * scale)
+        .drawImage(nx=ngrid, ny=ngrid, scale=scale)
+        .array
+    )
+
+    # force detection at center
+    coords = np.array(
+        [(ngrid / 2.0 + shift_y, ngrid / 2.0 + shift_x, True, 0)],
+        dtype=[
+            ("y", "f8"), ("x", "f8"), ("is_peak", "i4"), ("mask_value", "i4")
+        ]
+    )
+    return gal_array, psf_array, coords
+
+
+def do_test(scale, shift_x, shift_y):
+    sigma_arcsec = 0.53
+
+    gal_array, psf_array, coords = simulate_gal_psf(
+        scale, shift_x, shift_y,
+    )
+
+    ftask = anacal.fpfs.FpfsTask(
+        npix=64,
+        pixel_scale=scale,
+        sigma_arcsec=sigma_arcsec,
+        psf_array=psf_array,
+        do_detection=False,
+    )
+
+    src = ftask.run(
+        gal_array=gal_array,
+        psf=psf_array,
+        det=coords,
+    )
+
+    return rfn.structured_to_unstructured(src["data"])
+
+
+def test_shear_estimation():
+    data1 = do_test(0.2, 0.0, 0.0)
+    data2 = do_test(0.2, 2.31, 0.43)
+    np.testing.assert_almost_equal(data1, data2, decimal=4)
+
+    data2 = do_test(0.2, -2.35, 1.63)
+    np.testing.assert_almost_equal(data1, data2, decimal=4)
+
+    data2 = do_test(0.164, -0.5, 1.5)
+    np.testing.assert_almost_equal(data1, data2, decimal=4)
+    return
