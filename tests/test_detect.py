80d05d721b98f357baacfdf1f3b3fe3631d68d5f
Xiangchong Li <xiangchong.li@ipmu.jp>
2025-02-25 06:58:17 +0900

    add tests for detection

--- tests/test_detect.py
+++ tests/test_detect.py
@@ -12,14 +12,11 @@ kwargs = {
 }
 
 
-def test_detect(mag=27.5):
+def test_detect():
     nx = 64
     ny = 64
     scale = 0.2
     psf_fwhm = 0.7
-    sigma_arcsec = 0.5
-    klim = 100.0
-    flux = 10 ** ((30 - mag) / 2.5)
 
     # PSF
     psf_obj = galsim.Moffat(
@@ -40,7 +37,7 @@ def test_detect(mag=27.5):
     )
 
     obj0 = galsim.Exponential(half_light_radius=0.25)
-    obj0 = obj0.shear(e1=0.1, e2=-0.15).withFlux(flux)
+    obj0 = obj0.shear(e1=0.1, e2=-0.15).shift(0.05 * scale, 0.1 * scale)
 
     block = anacal.geometry.get_block_list(
         nx,
@@ -51,10 +48,11 @@ def test_detect(mag=27.5):
         scale,
     )[0]
 
-    def make_sim(g1, g2, angle=0.0):
+    def make_sim(g1, g2, angle=0.0, mag=27.5):
         obj = obj0.rotate(angle * galsim.degrees).shear(g1=g1, g2=g2)
         obj = obj.shift(0.5 * scale, 0.5 * scale)
-        obj = galsim.Convolve(psf_obj, obj)
+        flux = 10 ** ((30.0 - mag) / 2.5)
+        obj = galsim.Convolve(psf_obj, obj).withFlux(flux)
 
         # Create an empty image
         full_image = galsim.ImageF(ncol=nx, nrow=ny, scale=scale)
@@ -84,53 +82,62 @@ def test_detect(mag=27.5):
     assert cats[0].model.x1.v / scale == nx // 2
     assert cats[0].model.x2.v / scale == ny // 2
 
-    cat_1 = anacal.detector.find_peaks(
-        img_array=make_sim(g1=-0.02, g2=0.0),
-        psf_array=psf_array,
-        block=block,
-        noise_array=None,
-        image_bound=0,
-        **kwargs,
-    )[0]
+    for mag in np.arange(27.5, 28.3, 0.1):
+        cat_1 = anacal.detector.find_peaks(
+            img_array=make_sim(g1=-0.02, g2=0.0, mag=mag),
+            psf_array=psf_array,
+            block=block,
+            noise_array=None,
+            image_bound=0,
+            **kwargs,
+        )
+        assert len(cat_1) == 1
+        cat_1 = cat_1[0]
 
-    cat_2 = anacal.detector.find_peaks(
-        img_array=make_sim(g1=0.02, g2=0.0),
-        psf_array=psf_array,
-        block=block,
-        noise_array=None,
-        image_bound=0,
-        **kwargs,
-    )[0]
+        cat_2 = anacal.detector.find_peaks(
+            img_array=make_sim(g1=0.02, g2=0.0, mag=mag),
+            psf_array=psf_array,
+            block=block,
+            noise_array=None,
+            image_bound=0,
+            **kwargs,
+        )
+        assert len(cat_2) == 1
+        cat_2 = cat_2[0]
 
-    np.testing.assert_approx_equal(
-        (cat_2.wdet.v - cat_1.wdet.v) / 0.04,
-        (cat_2.wdet.g1 + cat_1.wdet.g1) / 2.0,
-        3,
-    )
+        np.testing.assert_approx_equal(
+            (cat_2.wdet.v - cat_1.wdet.v) / 0.04,
+            (cat_2.wdet.g1 + cat_1.wdet.g1) / 2.0,
+            2,
+        )
 
-    cat_1 = anacal.detector.find_peaks(
-        img_array=make_sim(g2=-0.02, g1=0.0),
-        psf_array=psf_array,
-        block=block,
-        noise_array=None,
-        image_bound=0,
-        **kwargs,
-    )[0]
+        cat_1 = anacal.detector.find_peaks(
+            img_array=make_sim(g2=-0.02, g1=0.0, mag=mag),
+            psf_array=psf_array,
+            block=block,
+            noise_array=None,
+            image_bound=0,
+            **kwargs,
+        )
+        assert len(cat_1) == 1
+        cat_1 = cat_1[0]
 
-    cat_2 = anacal.detector.find_peaks(
-        img_array=make_sim(g2=0.02, g1=0.0),
-        psf_array=psf_array,
-        block=block,
-        noise_array=None,
-        image_bound=0,
-        **kwargs,
-    )[0]
+        cat_2 = anacal.detector.find_peaks(
+            img_array=make_sim(g2=0.02, g1=0.0, mag=mag),
+            psf_array=psf_array,
+            block=block,
+            noise_array=None,
+            image_bound=0,
+            **kwargs,
+        )
+        assert len(cat_2) == 1
+        cat_2 = cat_2[0]
 
-    np.testing.assert_approx_equal(
-        (cat_2.wdet.v - cat_1.wdet.v) / 0.04,
-        (cat_2.wdet.g2 + cat_1.wdet.g2) / 2.0,
-        3,
-    )
+        np.testing.assert_approx_equal(
+            (cat_2.wdet.v - cat_1.wdet.v) / 0.04,
+            (cat_2.wdet.g2 + cat_1.wdet.g2) / 2.0,
+            2,
+        )
 
     for ang in np.random.random(10) * 360:
         cat_1 = anacal.detector.find_peaks(
