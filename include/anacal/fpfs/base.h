c8b3f60199172ec1c7506048ed4e4da63008a818
Xiangchong Li <12228372+mr-superonion@users.noreply.github.com>
2025-09-27 12:36:22 -0400

    Silence idx4 unused parameter warning

--- include/anacal/fpfs/base.h
+++ include/anacal/fpfs/base.h
@@ -9,6 +9,13 @@
 namespace anacal {
     inline constexpr double fpfs_det_sigma2 = 0.04;
     inline constexpr double fpfs_cut_sigma_ratio = 1.6;
+
+    namespace fpfs_detail {
+        inline constexpr int idx4(int n, int m, int norder) noexcept {
+            static_cast<void>(norder);
+            return n * 4 + m;
+        }
+    }
 }
 
 #endif // ANACAL_FPFS_BASE_H
