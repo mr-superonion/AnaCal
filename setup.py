9ab8c961fec89b7462b8bfc2a2964efc9fb4dce6
Xiangchong Li <12228372+mr-superonion@users.noreply.github.com>
2025-10-22 21:06:38 -0400

    Add Windows wheel builds to PyPI workflow

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import ctypes
+import glob
 import os
 import sys
 
@@ -23,6 +24,7 @@ def find_fftw_lib():
     if "FFTW_DIR" in os.environ:
         try_libdirs.append(os.environ["FFTW_DIR"])
         try_libdirs.append(os.path.join(os.environ["FFTW_DIR"], "lib"))
+        try_libdirs.append(os.path.join(os.environ["FFTW_DIR"], "bin"))
 
     # Add the python system library directory.
     try_libdirs.append(distutils.sysconfig.get_config_var("LIBDIR"))
@@ -42,13 +44,19 @@ def find_fftw_lib():
     # The user's home directory is often a good place to check.
     try_libdirs.append(os.path.join(os.path.expanduser("~"), "lib"))
     if sys.platform == "darwin":
-        lib_ext = ".dylib"
+        lib_names = ["libfftw3.dylib"]
+    elif sys.platform == "win32":
+        lib_names = [
+            "libfftw3-3.dll",
+            "fftw3-3.dll",
+            "libfftw3.dll",
+            "fftw3.dll",
+        ]
     else:
-        lib_ext = ".so"
-    name = "libfftw3" + lib_ext
+        lib_names = ["libfftw3.so"]
     tried_dirs = set()  # Keep track, so we don't try the same thing twice.
     for dir in try_libdirs:
-        if dir == "":
+        if not dir:
             continue  # This messes things up if it's in there.
         if dir in tried_dirs:
             continue
@@ -56,46 +64,89 @@ def find_fftw_lib():
             tried_dirs.add(dir)
         if not os.path.isdir(dir):
             continue
-        libpath = os.path.join(dir, name)
-        if not os.path.isfile(libpath):
-            continue
-        try:
-            lib = ctypes.cdll.LoadLibrary(libpath)
-            return libpath
-        except OSError as e:
-            # Some places use lib64 rather than/in addition to lib.
-            if dir.endswith("lib") and os.path.isdir(dir + "64"):
-                dir += "64"
-                try:
-                    libpath = os.path.join(dir, name)
+        for name in lib_names:
+            libpath = os.path.join(dir, name)
+            if not os.path.isfile(libpath):
+                continue
+            try:
+                ctypes.cdll.LoadLibrary(libpath)
+                return libpath
+            except OSError:
+                # Some places use lib64 rather than/in addition to lib.
+                if dir.endswith("lib") and os.path.isdir(dir + "64"):
+                    dir64 = dir + "64"
+                    libpath = os.path.join(dir64, name)
                     if not os.path.isfile(libpath):
                         continue
-                    lib = ctypes.cdll.LoadLibrary(libpath)
-                    return libpath
-                except OSError:
-                    pass
+                    try:
+                        ctypes.cdll.LoadLibrary(libpath)
+                        return libpath
+                    except OSError:
+                        pass
 
     # If we didn't find it anywhere, but the user has set FFTW_DIR, trust it.
     if "FFTW_DIR" in os.environ:
-        libpath = os.path.join(os.environ["FFTW_DIR"], name)
+        # Try to locate any of the expected names inside FFTW_DIR
+        for name in lib_names:
+            libpath = os.path.join(os.environ["FFTW_DIR"], name)
+            if os.path.isfile(libpath):
+                return libpath
         print("WARNING:")
-        print("Could not find an installed fftw3 library named %s" % name)
+        print("Could not find an installed fftw3 library.")
         print(
-            "Trust the provided FFTW_DIR=%s for the library location." % libpath
+            "Trust the provided FFTW_DIR=%s for the library location." % os.environ["FFTW_DIR"]
         )
         print("If this is incorrect, you may have errors later when linking.")
-        return libpath
+        return os.environ["FFTW_DIR"]
 
 
 fftw_lib = find_fftw_lib()
+library_dirs = []
+libraries = ["fftw3"]
 if fftw_lib is not None:
-    fftw_libpath, fftw_libname = os.path.split(fftw_lib)
-    include_dirs.append(
-        os.path.join(
-            os.path.split(fftw_libpath)[0],
-            "include",
-        )
-    )
+    if os.path.isfile(fftw_lib):
+        fftw_libpath, fftw_libname = os.path.split(fftw_lib)
+        include_roots = {os.path.split(fftw_libpath)[0], fftw_libpath}
+    else:
+        fftw_libpath = fftw_lib
+        fftw_libname = ""
+        include_roots = {fftw_libpath, os.path.split(fftw_libpath)[0]}
+
+    for include_root in include_roots:
+        candidate_include = os.path.join(include_root, "include")
+        if os.path.isdir(candidate_include) and candidate_include not in include_dirs:
+            include_dirs.append(candidate_include)
+
+    if os.path.isdir(fftw_libpath) and fftw_libpath not in library_dirs:
+        library_dirs.append(fftw_libpath)
+
+    if sys.platform == "win32":
+        lib_dir = os.path.join(os.path.split(fftw_libpath)[0], "lib")
+        if os.path.isdir(lib_dir):
+            if lib_dir not in library_dirs:
+                library_dirs.append(lib_dir)
+            lib_candidates = glob.glob(os.path.join(lib_dir, "*fftw*.lib"))
+            if lib_candidates:
+                libraries = [os.path.splitext(os.path.basename(lib_candidates[0]))[0]]
+            elif fftw_libname:
+                libraries = [os.path.splitext(fftw_libname)[0]]
+        elif fftw_libname:
+            libraries = [os.path.splitext(fftw_libname)[0]]
+
+extra_compile_args = [
+    "-Wall",
+    "-Wextra",
+    "-Wdeprecated-declarations",
+    "-std=c++17",
+    "-fopenmp",
+    "-O3",
+    "-fvisibility=hidden",
+]
+extra_link_args = ["-flto", "-fopenmp"]
+
+if sys.platform == "win32":
+    extra_compile_args = ["/std:c++17", "/O2", "/openmp", "/EHsc"]
+    extra_link_args = ["/openmp"]
 
 ext_modules = []
 ext_modules.append(
@@ -119,18 +170,11 @@ ext_modules.append(
             "src/task.cpp",
         ],
         include_dirs=include_dirs,
-        libraries=["fftw3"],
+        libraries=libraries,
+        library_dirs=library_dirs,
         language="c++",
-        extra_compile_args=[
-            "-Wall",
-            "-Wextra",
-            "-Wdeprecated-declarations",
-            "-std=c++17",
-            "-fopenmp",
-            "-O3",
-            "-fvisibility=hidden",
-        ],
-        extra_link_args=["-flto", "-fopenmp"],
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
     )
 )
 
